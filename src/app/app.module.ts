7dfa3a94d96e9a6767f7c410619169194f33ed0e
Andy Tung <andyytung@gmail.com>
2016-11-12 03:44:40 -0800

    facebook auth setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,21 @@ import { LoginComponent } from './login/login.component';
 import { GameComponent } from './game/game.component';
 import { RoleComponent } from './game/role/role.component';
 import { CardBaseComponent } from './shared/card-base/card-base.component';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDzV4LHW-Z177LuCopYh7Vsd65AShwU3F8",
-    authDomain: "sherlock-3c9fd.firebaseapp.com",
-    databaseURL: "https://sherlock-3c9fd.firebaseio.com",
-    storageBucket: "sherlock-3c9fd.appspot.com",
-    messagingSenderId: "578690943646"
-  };
+  apiKey: "AIzaSyDzV4LHW-Z177LuCopYh7Vsd65AShwU3F8",
+  authDomain: "sherlock-3c9fd.firebaseapp.com",
+  databaseURL: "https://sherlock-3c9fd.firebaseio.com",
+  storageBucket: "sherlock-3c9fd.appspot.com",
+  messagingSenderId: "578690943646"
+};
 
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Facebook,
+  method: AuthMethods.Redirect
+}
 
 @NgModule({
   declarations: [
@@ -34,7 +38,7 @@ var firebaseConfig = {
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
