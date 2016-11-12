930261168c4e8a176924990f9319c361b81b9e41
eleanorwong <eleanorwong@live.ca>
2016-11-12 06:12:46 -0800

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,14 @@ import { LoginComponent }   from './login/login.component';
 import { GameComponent }      from './game/game.component';
 import { CreateComponent } from './game/create/create.component';
 import { RoleComponent }      from './game/role/role.component';
+import { CycleComponent } from './game/cycle/cycle.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login',  component: LoginComponent },
-    { path: 'game', component: GameComponent,
-        children: [
-            { path: '' },
-            {':id', component: CycleComponent },
-            { path: 'create', component: CreateComponent },
-            { path: 'role', component: RoleComponent }
-        ]
-    }
+    { path: 'create', component: CreateComponent },
+    { path: 'role', component: RoleComponent },
+    { path: 'game/:id', component: GameComponent }
 ];
 
 @NgModule({
