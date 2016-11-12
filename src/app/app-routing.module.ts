7ce737f870cebb5888b67a25ac4cafa51b977c67
eleanorwong <eleanorwong@live.ca>
2016-11-12 02:57:48 -0800

    Adding Create Game Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent }   from './login/login.component';
 import { GameComponent }      from './game/game.component';
+import { CreateComponent } from './game/create/create.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login',  component: LoginComponent },
     { path: 'game', component: GameComponent,
-    children: [
-        { path: '' }
-    ]}
+        children: [
+            { path: '' },
+            { path: 'create', component: CreateComponent }
+        ]
+    }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GameComponent } from './game/game.component';
+import { CreateComponent } from './game/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GameComponent
+    GameComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
