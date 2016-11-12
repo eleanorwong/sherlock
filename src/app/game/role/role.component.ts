c0da5796435c7ec271e06a63fa61eb0a94c3f1e8
kvmu <kevinmultani@gmail.com>
2016-11-12 03:56:04 -0800

    card-base and role component

--- src/app/game/role/role.component.ts
+++ src/app/game/role/role.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role.component.scss']
 })
 export class RoleComponent implements OnInit {
-
+  isRoleCard = true;
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/card-base/card-base.component.ts
+++ src/app/shared/card-base/card-base.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-base.component.scss']
 })
 export class CardBaseComponent implements OnInit {
+  isRoleCard = true;
 
   constructor() { }
 
