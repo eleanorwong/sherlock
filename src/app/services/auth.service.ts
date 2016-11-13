7339f1bde1cd023e871ade441d78fce10471a6a1
eleanorwong <eleanorwong@live.ca>
2016-11-12 18:31:53 -0800

    Fixing routing bug

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
         this.router.navigate(['login']);
       } else if(this.router.url === '/login' && auth != null) {
           this.af.database.object("users/"+this.getUID()+"/activeGame/").subscribe((game) => {
-              if(game !== undefined && game !== "") {
+              if(game.$value !== "" && game !== undefined && game !== null) {
                   this.router.navigate([game.$value+'/lobby']);
               } else {
                   this.router.navigate(['menu']);
